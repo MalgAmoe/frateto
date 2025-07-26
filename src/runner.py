3808b212b25713ac8848559aa70ad5f99ed4d653
MalgAmoe <malgamoe@protonmail.com>
2025-07-26 21:44:50 +0200

    adding the worst session management in history

--- src/runner.py
+++ src/runner.py
@@ -6,16 +6,22 @@ from google.genai import types
 APP_NAME = "frateto"
 
 session_service = InMemorySessionService()
+runner = Runner(agent=root_agent, session_service=session_service, app_name=APP_NAME)
 
 async def run_agent(session_id: str, user_id: str, user_message: str) -> str:
     """Simple function to run the agent and return the response"""
 
-    await session_service.create_session(
+    session = await session_service.get_session(
+        app_name=APP_NAME,
+        user_id=user_id,
+        session_id=session_id
+    )
+    if not session:
+        await session_service.create_session(
             app_name=APP_NAME,
             user_id=user_id,
             session_id=session_id
         )
-    runner = Runner(agent=root_agent, session_service=session_service, app_name=APP_NAME)
 
     content = types.Content(
         role='user',
